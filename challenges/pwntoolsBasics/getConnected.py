f152237801b87dfc092a518d2a01db6c85daf118
ColineVL <coline.vanleeuwen@gmail.com>
2022-01-18 20:57:45 +0100

    challenge echo completed

--- challenges/pwntoolsBasics/getConnected.py
+++ challenges/pwntoolsBasics/getConnected.py
@@ -2,7 +2,9 @@ from pwn import *
 
 rem = remote("35.195.130.106", 17100)
 
-reply_as_a_string = rem.recvall().strip().decode()
+# reply = rem.readline()
+reply = rem.recvall()
+reply_as_a_string = reply.strip().decode()
 print(reply_as_a_string)
 
 rem.close()
--- challenges/pwntoolsBasics/simpleEchoServer.py
+++ challenges/pwntoolsBasics/simpleEchoServer.py
@@ -2,9 +2,30 @@ from pwn import *
 
 rem = remote("35.195.130.106", 17101)
 
-reply_as_a_string = rem.recvall().strip().decode()
-print(reply_as_a_string)
+# Instructions
+for i in range(2):
+    reply = rem.readline()
+    reply_as_a_string = reply.strip().decode()
+    print(f"REC {i} {reply_as_a_string}")
 
-rem.send("GET /\r\n\r\n")
-reply = rem.readline()
-print(reply)
+stop = False
+j = 0
+
+# Receive sentences in latin
+while not stop:
+    reply = rem.readline()
+    reply_as_a_string = reply.strip().decode()
+
+    if "Wrong" in reply_as_a_string:
+        print(f"NOPE {rem.recvall().strip().decode()}")
+
+    elif "Well done" in reply_as_a_string:
+        stop = True
+        print(reply_as_a_string)
+        print(f"{rem.recvall().strip().decode()}")
+
+    else:
+        print(f"REC2 {j} {reply_as_a_string}")
+        rem.send(reply_as_a_string + "\r\n")
+
+    j += 1
