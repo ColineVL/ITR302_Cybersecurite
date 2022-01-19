7ca817c59d0d647ada6101c456ed86db94a58787
ColineVL <coline.vanleeuwen@gmail.com>
2022-01-19 18:38:07 +0100

    i can find a collision for 16 bits

--- challenges/HashInversion/findCollisionFunction.py
+++ challenges/HashInversion/findCollisionFunction.py
@@ -1,34 +1,44 @@
-import string
-import random
 import hashlib
 
 
 def findCollision():
+    N = 16  # 48 premiers bits
+    n = 4
 
-    # Je crée un dict : 48 premiers bits du hash en SHA-256 -> message
+    # Je crée un dict : N premiers bits du hash en SHA-256 -> message
     myDict = {}
 
     flagFound = False
     msg1 = ""
     msg2 = ""
 
-    while not flagFound:
-        # Je crée un message
-        length_of_string = 8
-        message = "".join(
-            random.choice(string.ascii_letters + string.digits)
-            for _ in range(length_of_string)
-        )
-        print(message)
+    # Je récupère plein de mots depuis le fichier words_alpha.txt
+    myfile = open("./useful/words_alpha.txt", "r")
+    array = myfile.readlines()
+    myfile.close()
 
-        # TODO plutot utiliser words-alpha.txt ???????
+    # On enlève les \n
+    for i in range(len(array) - 1):
+        array[i] = array[i].strip("\n")
 
-        # Je vérifie qu'on n'est pas déjà tombés sur ce message
-        if not message in myDict.values():
+    # On ajoute un peu des mots pour avoir un array plus long
+    array2 = []
+    for message in array:
+        for chiffre in range(50):
+            array2.append(f"{message}{chiffre}")
+    resultArray = array2 + array
+
+    print(f"len du fichier : {len(array)}")
+    print(f"len de ce qu'on ajoute : {len(array2)}")
+    print(f"len total : {len(resultArray)}")
+
+    # C'est parti
+    for message in resultArray:
+        if not flagFound:
 
             # Je calcule le hash en SHA-256 du message
             hash = hashlib.sha256(message.encode()).hexdigest()
-            firstChars = hash[:48]
+            firstChars = hash[:n]
 
             # Je regarde si ce hash est dans le dict
             if firstChars in myDict:
@@ -39,11 +49,22 @@ def findCollision():
             else:
                 myDict[firstChars] = message
 
-    print(f"{msg1}, {msg2}")
-    hash1 = hashlib.sha256(msg1.encode()).hexdigest()[:4]
-    hash2 = hashlib.sha256(msg2.encode()).hexdigest()[:4]
-    print(f"{hash1}, {hash2}")
-    return msg1, msg2
+    print(f"len du dict : {len(myDict.keys())}")
+
+    if flagFound:
+        print("Collision trouvée")
+        print(f"Messages : {msg1}, {msg2}")
+        hash1 = hashlib.sha256(msg1.encode()).hexdigest()[:12]
+        hash2 = hashlib.sha256(msg2.encode()).hexdigest()[:12]
+        print(f"Hashes : {hash1}, {hash2}")
+        return msg1, msg2
+
+    else:
+        print("Aucune collision trouvée")
+        return "", ""
 
 
 findCollision()
+
+# Nb possibilités pour un hash de 48 bits : 281474976710656
+# J'en explore 1480412
